8239067b8593e975c25c95829f254b7742cb5a9f
FLumine87 <787249795@qq.com>
2026-04-28 21:51:01 +0800

    refactor(calibration): 简化标定采集程序并移除串口依赖
    移除串口相关代码，简化标定采集流程
    修改标定板识别为7x7圆点阵列
    优化图像保存逻辑，移除四元数保存功能

--- src/sp_vision_25-main/autostart.sh
+++ src/sp_vision_25-main/autostart.sh
@@ -1,10 +1,10 @@
+#!/bin/bash
+
 sleep 5
-cd ~/Desktop/sp_vision_25/
+cd ~/sp_vision_25-main/
 screen \
     -L \
     -Logfile logs/$(date "+%Y-%m-%d_%H-%M-%S").screenlog \
     -d \
     -m \
     bash -c "./watchdog.sh"
-
-# ./build/auto_aim_debug_mpc_rosdebug configs/standard3.yaml 
