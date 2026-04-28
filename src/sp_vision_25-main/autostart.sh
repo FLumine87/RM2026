08ca75cd2a91e259f72d9c86049edd109270ccd5
FLumine87 <787249795@qq.com>
2026-04-28 22:31:14 +0800

    fix(autostart): 修正脚本路径问题并添加注释

--- src/sp_vision_25-main/autostart.sh
+++ src/sp_vision_25-main/autostart.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 sleep 5
-cd ~/sp_vision_25-main/
+cd "$(dirname "$0")"
 
 # 使用 xterm 打开终端窗口运行 watchdog
 # -hold 保持终端窗口不关闭
 # -e 指定要执行的命令
 xterm -hold -e "bash watchdog.sh"
+
+# sed -i 's/\r$//' /home/a/sp_vision_25-main_2/sp_vision_25-main/autostart.sh
